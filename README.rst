843c074dce6437af5f8d7c30c1d0d93751d11f1d
Roy Zheng <roycraft3@gmail.com>
2016-11-28 19:26:49 -0800

    Fix PyPI setup
    PyPI doesn't like uploading the README for some reason. Fixed, and
    added rst formatted README.

---
+++ README.rst
@@ -0,0 +1,23 @@
+Beeradvocatereviews
+===================
+
+| Beeradvocatereviews is a module that lets you export a user's reviews
+| from Beer Advocate.
+
+Usage is very simple. To install:
+
+::
+
+    pip install beeradvocatereviews
+
+To run
+
+::
+
+    beeradvocatereviews MyUsername NumberOfReviewsIHave
+
+e.g.
+
+::
+
+    beeradvocatereviews TheHefe 231
--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='beeradvocatereviews',
-    version='1.0.0',
+    version='1.0.2',
 
     description='Module to export user reviews',
     long_description=long_description,
